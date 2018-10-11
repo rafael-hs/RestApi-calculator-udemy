a4f65ef4d1eb56dc60ff2e85eed7d6989f084418
rafael -HS <rafaelzinhohs@outlook.com>
2018-10-11 10:57:41 -0300

    iniciando projeto add operacao sum

---
+++ RestApiCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApi_Udemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // GET api/values/5/5
+        [HttpGet("{fistNumber}/{secondNumber}")]
+        public IActionResult Sum(string fistNumber, string secondNumber)
+        {
+            if (isNumeric(fistNumber) && isNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(fistNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool isNumeric(string strNumber)
+        {
+            double number;
+
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
