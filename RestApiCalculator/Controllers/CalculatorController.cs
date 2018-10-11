f754228ddb5dfa5ac7de053a36eb0a0ceb012985
rafael -HS <rafaelzinhohs@outlook.com>
2018-10-11 14:27:59 -0300

    adicionando as funcionalidades de uma calculadora em rest

--- RestApiCalculator/Controllers/CalculatorController.cs
+++ RestApiCalculator/Controllers/CalculatorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Emit;
 
 namespace RestApi_Udemy.Controllers
 {
@@ -10,14 +11,76 @@ namespace RestApi_Udemy.Controllers
     [ApiController]
     public class CalculatorController : ControllerBase
     {
-        // GET api/values/5/5
-        [HttpGet("{fistNumber}/{secondNumber}")]
+        // GET api/sum/5/5
+        [HttpGet("sum/{fistNumber}/{secondNumber}")]
         public IActionResult Sum(string fistNumber, string secondNumber)
         {
             if (isNumeric(fistNumber) && isNumeric(secondNumber))
             {
                 var sum = ConvertToDecimal(fistNumber) + ConvertToDecimal(secondNumber);
                 return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+        //GET api/sub/5/5
+        [HttpGet("sub/{fistNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string fistNumber, string secondNumber)
+        {
+            if(isNumeric(fistNumber) && isNumeric(secondNumber))
+            {
+                var subtraction = ConvertToDecimal(fistNumber) - ConvertToDecimal(secondNumber);
+                return Ok(subtraction.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+        //GET api/multi/5/5
+        [HttpGet("multi/{fistNumber}/{secondNumber}")]
+        public IActionResult Multi(string fistNumber, string secondNumber)
+        {
+            if(isNumeric(fistNumber) && isNumeric(secondNumber))
+            {
+                var multi = ConvertToDecimal(fistNumber) * ConvertToDecimal(secondNumber);
+                return Ok(multi.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+        //GET api/div/5/5
+        [HttpGet("div/{fistNumber}/{secondNumber}")]
+        public IActionResult Division(string fistNumber, string secondNumber)
+        {
+            if (isNumeric(fistNumber) && isNumeric(secondNumber))
+            {
+                var division = ConvertToDecimal(fistNumber) / ConvertToDecimal(secondNumber);
+                return Ok(division.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        //GET api/mean/5/5
+        [HttpGet("mean/{fistNumber}/{secondNumber}")]
+        public IActionResult Mean(string fistNumber, string secondNumber)
+        {
+            if (isNumeric(fistNumber) && isNumeric(secondNumber))
+            {
+                var mean = ConvertToDecimal(fistNumber) + ConvertToDecimal(secondNumber) / 2;
+                return Ok(mean.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        //GET api/square-root/5
+        [HttpGet("square-root/{fistNumber}")]
+        public IActionResult SquareRoot(string fistNumber, string secondNumber)
+        {
+            if (isNumeric(fistNumber))
+            {
+                var sqr = Math.Sqrt((double)ConvertToDecimal(fistNumber));
+                return Ok(sqr.ToString());
             }
 
             return BadRequest("Invalid input");
